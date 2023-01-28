ad749171ea1062088f22d64eb31f3e95df7494c1
Adrian <adrj67@gmail.com>
2023-01-28 07:47:02 -0300

    Primer commit. Ejercicios tema 3. Java Basico

---
+++ src/ej3javabasico/Ej3JavaBasico.java
@@ -0,0 +1,13 @@
+
+package ej3javabasico;
+
+public class Ej3JavaBasico {
+
+    public static void main(String[] args) {
+        String [] nombres = {"Pepe", "Juanito", "Ana", "Ruperta", "Antonio"};
+        for(String nombre : nombres){
+          System.out.print(nombre + " ");
+        }
+    }
+    
+}
